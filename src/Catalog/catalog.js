6db33e9655e723d9bfc53a66a81f598073dc4553
Strider311 <saif_kh31@outlook.com>
2022-05-03 23:10:00 +0400

    same as last commit

--- src/Catalog/catalog.js
+++ src/Catalog/catalog.js
@@ -11,13 +11,13 @@ const Catalog = () => {
           <Link to="/catalog/soaps">
             <section className="product-family">
               <h3>Soap Collection</h3>
-              <img src="images/Savon_Bio-01 .jpg" alt="Soap collection"></img>
+              <img src="images/Savon_Bio-01.jpg" alt="Soap collection"></img>
             </section>
           </Link>
           <Link to="/">
             <section className="product-family">
               <h3>Beauty products</h3>
-              <img src="images/Savon_Bio-02 .jpg" alt="Soap collection"></img>
+              <img src="images/Savon_Bio-02.jpg" alt="Soap collection"></img>
             </section>
           </Link>
         </div>
